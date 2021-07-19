0de19c639782e3bc5f02453918d761b38e1bb9e3
apostolos-e <apostolos.eleftheriou@citrix.com>
2021-07-19 12:05:10 +0300

    Made changes v1

--- script.sh
+++ script.sh
@@ -2,37 +2,39 @@
 #./script.sh -f <filename> to specify file to write the output.
 #./script.sh -s <filename> for cron scheduling.
 
-function commands(){
+function single(){
+    date +"[%m-%d-%Y %T]:" | tr '\n' '\t' >> $FILE
     uptime | awk -F'[a-z]:' '{printf "%s\t", $2}' >> $FILE
     free -m | awk 'NR==2{printf "%s/%sMB(%.2f%%)\t", $3,$2,$3*100/$2 }' >> $FILE
     df -h | awk '$NF=="/"{printf "%d/%dGB(%s)\t", $3,$2,$5}' >> $FILE
     ps aux | awk '{printf "%s\n", $8}' | grep -c Z >> $FILE
 }
 
-function std_out(){
+function periodic(){
     while [ true ]
     do
-        commands
+        single
         cat $FILE
         sleep 10s
         clear
     done
 }
 
-if [ $# -eq 0 ] 
+if [ $# -eq 0 ]
 then
-    echo "Invalid argument please pass -f or -s."
+    echo "Invalid argument please pass -f <filename> or -s <filename>."
 fi
-
-while getopts f:s flag
+#log rotation
+#/etc/logrotate.d/dpkg
+while getopts :f:s: flag
 do
     case "${flag}" in
         f) 
             FILE="/home/$USER/${OPTARG}"
-            std_out;;
+            periodic;;
         s) 
-            FILE="/var/log/${OPTARG}"
-            commands;;
-        *) echo "Invalid argument";;
+            FILE="/home/$USER/${OPTARG}"
+            single;;
+        *) echo "Invalid argument, no filename.";;
     esac
 done
